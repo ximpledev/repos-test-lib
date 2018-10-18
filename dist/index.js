d94c4f3418dc7a8b1e0cb1c25284dfba848ca93d
Jerry Lee <ximpledev@gmail.com>
2018-10-18 16:02:47 +0800

    add dist/

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function greet(name) {
+  return "Hi hi, " + name + "!";
+};
+
+exports.default = greet;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function greet(name) {
-  return `Hello, ${name}!`;
+  return `Hi hi, ${name}!`;
 };
 
 export default greet;
